d59590c8034b739ea3ce90c34fb34323e0c8a830
Jinge <jinge0829@gmail.com>
2024-10-30 10:35:46 -0400

    Update server.py to use permanent access keys.

--- server.py
+++ server.py
@@ -16,7 +16,6 @@ s3 = boto3.client(
     's3',
     aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
-    aws_session_token=os.environ.get('AWS_SESSION_TOKEN'),  # Ensure session token is included for temporary credentials
     region_name=os.environ.get('AWS_DEFAULT_REGION')
 )
 
